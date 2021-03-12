df83f1e58cf8c404d53ad1b2a4c77af2a4842e1d
彭国豪 <peng_gh@sea-level.com.cn>
2021-03-12 14:04:21 +0800

    1、增加docker-compose的初始化环境

--- kaleidoscope-common/src/main/java/com/pgh/kaleidoscope/common/constant/LauncherConstant.java
+++ kaleidoscope-common/src/main/java/com/pgh/kaleidoscope/common/constant/LauncherConstant.java
@@ -22,7 +22,7 @@ public interface LauncherConstant {
 	/**
 	 * nacos dev 地址
 	 */
-	String NACOS_DEV_ADDR = "127.0.0.1:8848";
+	String NACOS_DEV_ADDR = "192.168.0.125:8848";
 
 	/**
 	 * nacos prod 地址
